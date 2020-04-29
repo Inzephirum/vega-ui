5b416d15644c975a114a8c7c5a88e0a3d611a948
Oleg Denisov <c1n1kk@gmail.com>
2020-04-29 16:20:02 +0300

    fix
    Jira issue: VEGA-44

--- packages/components/tabs/src/Tabs.tsx
+++ packages/components/tabs/src/Tabs.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { cn as block } from '@gpn-design/uikit/__internal__/src/utils/bem';
 import { Tabs as BaseTabs } from '@gpn-design/uikit/Tabs';
+import { Badge } from '@vega-ui/badge';
 import { IconArrowLeft, IconArrowRight } from '@vega-ui/icons';
-import { block } from 'bem-cn';
 
+// import { block } from 'bem-cn';
 import './Tabs.css';
 
 type BaseTabsComponent = typeof BaseTabs;
@@ -14,6 +16,7 @@ export const Tabs: BaseTabsComponent = (props) => {
 
   return (
     <div className={cnTabs({ size })}>
+      <Badge className={cnTabs('Button', { mixed: true })} label="Кнопка" />
       <div className={cnTabs('ScrollLeft')}>
         <button type="button" className={cnTabs('ScrollButton')}>
           <IconArrowLeft />
@@ -21,7 +24,7 @@ export const Tabs: BaseTabsComponent = (props) => {
       </div>
       <div className={cnTabs('Inner')}>
         <div className={cnTabs('InnerContent')}>
-          <BaseTabs {...props} />
+          <BaseTabs {...props} className={cnTabs('Native', { align: 'center' })} />
         </div>
       </div>
       <div className={cnTabs('ScrollRight')}>
